13c3a4f6b1fb89c6f9c3e978bc2f7736785ff0de
Daniel_Glazer <58001286+danielglazer26@users.noreply.github.com>
2020-04-28 12:18:42 +0200

    Test changes

--- Miasta/src/Projekt/CitiesRelation.java
+++ Miasta/src/Projekt/CitiesRelation.java
@@ -8,6 +8,7 @@ class CitiesRelation  {
     protected void changeLevel(CitiesEvolution ce){
         if(city_influence<3)
         ce.getCity_table().get(1).decreaseLevel();
+        ce.getCity_table().get(2).decreaseLevel();
     }
 
 
